088ddcd7a12290b2ebe0c8b4606a22d45c13e232
Tomás NAVARRETE GUTIÉRREZ <tngTUDOR@users.noreply.github.com>
2023-06-25 20:21:29 +0200

    add calls page

--- components/Calls.vue
+++ components/Calls.vue
@@ -1,5 +1,17 @@
 <template>
-	<div>
-		<h1> Calls </h1>
-	</div>
+  <div class="container">
+    <h1>Calls</h1>
+
+    <p>
+      You can submit your own Brightcon 2023 idea for an interactive session:
+      workshop, presentation, you name it.
+    </p>
+    <p>
+      Please use the dedicated
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLSfnPVKruULk9cR_wC8OKu8QkSv1C7Dl7Pr0mxQELcnGUy37dw/viewform?pli=1"
+        >Google form </a
+      >to submit your idea
+    </p>
+  </div>
 </template>
