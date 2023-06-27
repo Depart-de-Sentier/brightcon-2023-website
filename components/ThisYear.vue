ffcccb015d88e0d0b68b058c82925868b0312e40
Tomás NAVARRETE GUTIÉRREZ <tomas.navarrete@list.lu>
2023-06-27 11:38:24 +0200

    links on text, not parenthesis

--- components/ThisYear.vue
+++ components/ThisYear.vue
@@ -20,38 +20,34 @@
         Jupyter Notebooks or similar technologies used so that the audience can
         follow along and explore your awesome software and data. This also makes
         for a fantastic experience for those attending, as each talk bridges
-        theory with practice and directly engages the audience. You can see talk
-        materials from last year online. (<a
-          href="https://github.com/Depart-de-Sentier/brightcon-2022-talks"
-        >
-          https://github.com/Depart-de-Sentier/brightcon-2022-talks</a
-        >)
+        theory with practice and directly engages the audience. You can see
+        <a href="https://github.com/Depart-de-Sentier/brightcon-2022-talks">
+          talk materials from last year online </a
+        >.
       </p>
 
       <p>
-        On September 16 & 17, we will have a hackathon with the HESTIA team (<a
-          href="https://www.hestia.earth/"
-          >https://www.hestia.earth/</a
-        >). HESTIA provides detailed and validated activity data on agriculture
-        and food supply chains (currently the data are from more than 400
-        published studies and 35,000 farms). HESTIA also makes available nearly
-        500 models to gap fill farm activity data (e.g., to quantify ruminant
-        grass consumption), to estimate agricultural emissions (e.g., N2O
-        emissions related to fertiliser application), and to quantify resource
-        uses. It is an open-source, open-access project and still in the
-        development and testing stage, but is already the largest open dataset
-        and model archive in this area. In the hackathon we will focus on
-        integrating HESTIA with open LCA software, and simplifying the process
-        of entering data into HESTIA.
+        On September 16 & 17, we will have a hackathon with the
+        <a href="https://www.hestia.earth/"> HESTIA team </a>. HESTIA provides
+        detailed and validated activity data on agriculture and food supply
+        chains (currently the data are from more than 400 published studies and
+        35,000 farms). HESTIA also makes available nearly 500 models to gap fill
+        farm activity data (e.g., to quantify ruminant grass consumption), to
+        estimate agricultural emissions (e.g., N2O emissions related to
+        fertiliser application), and to quantify resource uses. It is an
+        open-source, open-access project and still in the development and
+        testing stage, but is already the largest open dataset and model archive
+        in this area. In the hackathon we will focus on integrating HESTIA with
+        open LCA software, and simplifying the process of entering data into
+        HESTIA.
       </p>
       <p>
         The conference itself will take place from September 18-20, and will be
         a mix of talks, special sessions, and short courses. Being there in
         person is a lot of fun, but this year, with support from ecoinvent, we
-        are also offering online attendance (<a href="https://canapii.com"
-          >https://canapii.com</a
-        >). Physical attendance is limited to 80 people, and we expect it to
-        sell out, as it did last year.
+        are also offering <a href="https://canapii.com">online attendance</a>.
+        Physical attendance is limited to 80 people, and we expect it to sell
+        out, as it did last year.
       </p>
     </div>
   </div>
