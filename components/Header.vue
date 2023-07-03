ea2e03afba9e3888f4a6e6899320275ab4165451
Tomás NAVARRETE GUTIÉRREZ <tomas.navarrete@list.lu>
2023-07-03 12:22:31 +0200

    add logo to brand compo

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,10 @@
   <div class>
     <nav class="nav">
       <div class="nav-left">
-        <NuxtLink to="/" class="brand">Brightcon 2023 </NuxtLink>
+        <NuxtLink to="/" class="brand">
+	<NuxtImg src="/images/conf_logo.png" />
+	Brightcon 2023 
+	</NuxtLink>
       </div>
     </nav>
   </div>
