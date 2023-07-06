d3c7d1944c64dd879adce73c273982490b235d4f
Tomás NAVARRETE GUTIÉRREZ <tomas.navarrete@list.lu>
2023-07-06 12:41:33 +0200

    Fix typo.

--- components/Testimonials.vue
+++ components/Testimonials.vue
@@ -60,8 +60,9 @@ const quotes = ref([
     designation: "Assistant Professor at Aalborg University",
     reviewTitle: "",
     imgUrl: "/images/participants/agneta-ghose.jpg",
-    reviewMore:
-      " I attended BrightCon 2022 as a member of a panel discussion on semantic technologies in LCA. From my experience, BrighCon has been a great space to foster collaboration, learning and networking. Brightway users whilst growing is still a small community within LCA practitioners. BrightCon creates a space that gives its community the opportunity to engage in building a  resourceful network. I had the chance to meet representatives from the UNEP GLAD initiative at BrightCon last year and we are currently working on creating workflows for FAIR data sharing. <br /> Besides networking, I found all presentations were thought-provoking, engaging, and delivered with passion. It was evident that the speakers were genuinely invested in the open software movement and eager to share their knowledge and experiences. The knowledge I gained, and the overall experience exceeded my expectations and I would highly recommend to anyone interested in contributing to the use of open software in LCA as well as those eager to share their knowledge and experiences",
+    reviewMore: `I attended BrightCon 2022 as a member of a panel discussion on semantic technologies in LCA. From my experience, BrightCon has been a great space to foster collaboration, learning and networking. Brightway users whilst growing is still a small community within LCA practitioners. BrightCon creates a space that gives its community the opportunity to engage in building a  resourceful network. I had the chance to meet representatives from the UNEP GLAD initiative at BrightCon last year and we are currently working on creating workflows for FAIR data sharing. 
+
+      Besides networking, I found all presentations were thought-provoking, engaging, and delivered with passion. It was evident that the speakers were genuinely invested in the open software movement and eager to share their knowledge and experiences. The knowledge I gained, and the overall experience exceeded my expectations and I would highly recommend to anyone interested in contributing to the use of open software in LCA as well as those eager to share their knowledge and experiences`,
     linkedIn: "https://www.linkedin.com/in/agneta-ghose-8b48b321/",
     github: "https://github.com/agneta20",
   },
@@ -74,7 +75,7 @@ const quotes = ref([
     reviewMore:
       "The Brightcon 2022 provided a wonderful opportunity to delve deeper into Brightway and its latest features through a diverse program consisting of classes, conference sessions, and an engaging hackathon. Connecting with the kind, welcoming, and supportive Brightway community was a true highlight, making the Brightcon all the more enjoyable.",
     linkedIn: "https://www.linkedin.com/in/sarah-schmidt-82b187247/",
-    github: "https://github.com/SarahSchmidt94
+    github: "https://github.com/SarahSchmidt94",
   },
   {
     name: "Silvana Ovait",
