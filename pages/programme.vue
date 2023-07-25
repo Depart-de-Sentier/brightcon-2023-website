c8f64e98911485fd67701193667a37c25306240b
Tomás NAVARRETE GUTIÉRREZ <tomas.navarrete@list.lu>
2023-07-25 14:47:16 +0200

    [feat] Add tentative schedule for hackathon

--- pages/programme.vue
+++ pages/programme.vue
@@ -19,6 +19,36 @@
         open LCA software, and simplifying the process of entering data into
         HESTIA.
       </p>
+      <table class="styled-table">
+        <thead>
+          <th>Date</th>
+          <th>Time</th>
+          <th>Location</th>
+          <th>Event</th>
+        </thead>
+        <tbody>
+          <tr class="active-row">
+            <td>16/Sept/2023</td>
+            <td>09:30 - 10:00</td>
+            <td>Belvaux building of LIST</td>
+            <td>Registration</td>
+          </tr>
+          <tr class="active-row">
+            <td>16/Sept/2023</td>
+            <td>10:00 - 17:00</td>
+            <td>Belvaux building of LIST</td>
+            <td>hackathon</td>
+          </tr>
+          <tr class="active-row">
+            <td>17/Sept/2023</td>
+            <td>10:00 - 17:00</td>
+            <td>Belvaux building of LIST</td>
+            <td>hackathon</td>
+          </tr>
+        </tbody>
+
+        <tbody></tbody>
+      </table>
       <h2>Conference - Sept 18, 19 and 20</h2>
       <p>
         The conference will be a mix of talks, special sessions, and short
@@ -28,6 +58,7 @@
         is limited to 80 people, and we expect it to sell out, as it did last
         year.
       </p>
+
       The conference part of Brightcon 2023 will include (changes are still
       possible):
 
@@ -38,9 +69,44 @@
         <li>News from the Brightway development roadmap</li>
         <li>What's new in the Brightway documentation project</li>
         <li>Contribution analysis with Brightway and other modules</li>
-	<li>Full SQL database backend solution for Brightway</li>
-	<li>Overview of progress/plans for the AB</li>
+        <li>Full SQL database backend solution for Brightway</li>
+        <li>Overview of progress/plans for the AB</li>
       </ul>
     </div>
   </div>
 </template>
+
+<style scoped>
+.styled-table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+.styled-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+}
+.styled-table th,
+.styled-table td {
+  padding: 12px 15px;
+}
+.styled-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+
+.styled-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+
+.styled-table tbody tr:last-of-type {
+  border-bottom: 2px solid #009879;
+}
+.styled-table tbody tr.active-row {
+  font-weight: bold;
+  color: #009879;
+}
+</style>
